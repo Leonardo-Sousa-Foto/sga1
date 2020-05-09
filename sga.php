981ef7936b3823328d8e1a3933e2dbc5e7d6ed4b
Leonardo Sousa <leonardo.sousa@aluno.usj.edu.br>
2020-05-09 08:39:22 -0300

    anexado todos os arquivo trabalhado da pasta sga

---
+++ sga.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>SGA - Sistema de gerenciamento de arquivo</title>
+
+    </head>
+    <body>
+        <h3>Primeira atividade</h3>
+
+        <?php 
+          echo "Hello world !!";
+        ?>
+    </body>
+</html>
